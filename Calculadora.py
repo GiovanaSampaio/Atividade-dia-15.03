376d492b69bc01ff41af87c0f2098ea438c849b0
gamante <gisampaioamante@gmail.com>
2023-03-15 21:54:14 -0300

    Criação da função subtração

--- Calculadora.py
+++ Calculadora.py
@@ -4,8 +4,12 @@ print("Opção 2: subtração ")
 print("Opção 3: divisão ")
 print("Opção 4: multiplicação ")
 print("Opção 5: potenciação ")
-opcao = input("digite a opção desejada: ")
-if(opcao == 1):
-    numero1 = input("digite o primeiro número da soma: ")
-    numero2 = input("digite o segundo número da soma:  ")
+opcao = int(input("digite a opção desejada: "))
+if opcao == 1:
+    numero1 = float(input("digite o primeiro número da soma: "))
+    numero2 = float(input("digite o segundo número da soma:  "))
     soma = numero1 + numero2 
+elif opcao == 2:
+    numero1 = float(input("digite o primeiro número da subtração: "))
+    numero2 = float(input("digite o segundo número da subtração:  "))
+    subtracao = numero1 - numero2
