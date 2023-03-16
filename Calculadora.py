29dc35dc1db97e5efb7183d9ac11e551de844b87
gamante <gisampaioamante@gmail.com>
2023-03-15 21:47:57 -0300

    Criação da função soma

--- Calculadora.py
+++ Calculadora.py
@@ -5,3 +5,7 @@ print("Opção 3: divisão ")
 print("Opção 4: multiplicação ")
 print("Opção 5: potenciação ")
 opcao = input("digite a opção desejada: ")
+if(opcao == 1):
+    numero1 = input("digite o primeiro número da soma: ")
+    numero2 = input("digite o segundo número da soma:  ")
+    soma = numero1 + numero2 
