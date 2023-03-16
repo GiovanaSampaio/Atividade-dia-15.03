d0635b43a1bf1ba16f393adcf420cf561fe1f0e9
gamante <gisampaioamante@gmail.com>
2023-03-15 21:58:01 -0300

    Criação da opção divisão

--- Calculadora.py
+++ Calculadora.py
@@ -13,3 +13,8 @@ elif opcao == 2:
     numero1 = float(input("digite o primeiro número da subtração: "))
     numero2 = float(input("digite o segundo número da subtração:  "))
     subtracao = numero1 - numero2
+elif opcao == 3:
+    print("Lembrando que não pode dividir nenhum número por 0")
+    numero1 = float(input("digite o primeiro número da divisão: "))
+    numero2 = float(input("digite o segundo número da divisão:  "))
+    divisao = numero1 / numero2 
