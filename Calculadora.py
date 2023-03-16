f6d6c391fdde1453a8716e3184d3e096170bebee
gamante <gisampaioamante@gmail.com>
2023-03-15 21:44:21 -0300

    Criação das opções de entrada

--- Calculadora.py
+++ Calculadora.py
@@ -1,2 +1,7 @@
 nome = input("digite seu nome: ")
+print("Opção 1: soma ")
+print("Opção 2: subtração ")
+print("Opção 3: divisão ")
+print("Opção 4: multiplicação ")
+print("Opção 5: potenciação ")
 opcao = input("digite a opção desejada: ")
